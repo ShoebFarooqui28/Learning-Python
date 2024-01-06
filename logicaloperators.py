56d1f1f5456df78c22d0bb96b42020ddf998aceb
ShoebFarooqui28 <farooquishoeb2@gmail.com>
2024-01-06 07:38:16 +0530

    Changes

--- logicaloperators.py
+++ logicaloperators.py
@@ -1,1 +1,13 @@
+temp = int(input("What's the temperature right now?\n"))
+
+if temp <= 30 and temp >= 15 :
+    print("It's Moderate out there!")
+
+elif temp <= 0 and temp >= -15 :
+    print("It's Freezing out there!")
     
+elif temp > 30 : 
+    print("It's Extremly Hot out there!")
+
+else :
+    print("Its Breezy out there!")
