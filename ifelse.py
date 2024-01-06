a521dcc82d16734b1a209da90663128a4fa0a4b3
ShoebFarooqui28 <farooquishoeb2@gmail.com>
2024-01-06 07:06:42 +0530

    Changes

--- ifelse.py
+++ ifelse.py
@@ -1,6 +1,12 @@
-age = 17
-if age > 18 :
-    print("You are an Adult")
-    
-else :
-    print("You are not an Adult")
+age = int(input("What is your age?\n"))
+
+if age >= 75 :
+    print("You are a booomer!")
+
+elif age >= 18 :
+    print("You are an Adult") 
+       
+elif age <= 0 :
+    print("You are not even born")
+
+else : print("You are a child")    
--- stringslicing.py
+++ stringslicing.py
@@ -3,6 +3,17 @@ name = "Shoeb Farooqui"
 first_name = name[:5]
 last_name = name[6:]
 step_name = name[0:14:4]
+reversed_name = name[::-1]
 print(first_name)
 print(last_name)
 print(step_name)
+print(reversed_name)
+
+website = "www.google.com"
+website2 = "www.facebook.com"
+website3 = "www.instagram.com"
+
+slice = slice(4,-4)
+print(website[slice])
+print(website2[slice])
+print(website3[slice])
