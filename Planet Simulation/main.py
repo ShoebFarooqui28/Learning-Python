08ec7130f896b2535383cd6cf8f45596595c011d
Shoeb Farooqui <farooquishoeb2@gmail.com>
2024-11-09 12:26:52 +0530

    Created SUN

--- Planet Simulation/main.py
+++ Planet Simulation/main.py
@@ -1,23 +1,66 @@
 import pygame
 import math
+
 pygame.init()
 
-WIDTH, HEIGHT = 800, 800
-
+WIDTH = 800
+HEIGHT = 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-
 pygame.display.set_caption("Planet Simulation")
+YELLOW = (255, 191, 0)
 
+class Planet:
+    AU = 146.6e6 * 1000
+    G = 6.674228e-11
+    
+    #1 AU = 100 px
+    SCALE = 250 / AU   
+
+    # 1 Day
+    TIMESTEP = 3600*24 
+    
+    def __init__(self, x, y, radius, color, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+        self.x_vel = 0
+        self.y_vel = 0
+        
+        self.sun = False
+        self.distanceToSun = 0
+        self.orbit = []
+        
+    def draw(self, win):
+        x = self.x * self.SCALE + WIDTH / 2
+        y = self.y * self.SCALE + HEIGHT / 2
+        pygame.draw.circle(win,self.color, (x,y), self.radius) 
+        
+        
+        
 def main():
     run = True
-
+    clock = pygame.time.Clock()
+    
+    sun = Planet(0,0, 30, YELLOW, 1.989e30)
+    sun.sun = True
+    
+    planets = [sun]
+    
     while run:
-        for event in pygame.get.();
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        for planet in planets:
+            planet.draw(WIN)
 
-    pygame.QUIT()
+        pygame.display.update()
+    pygame.quit()
+    
+main()
+
+
+
         
-
-
-
+        
