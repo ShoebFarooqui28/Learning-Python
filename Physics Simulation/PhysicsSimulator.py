1f703614adcf8828ab589e659f5d47d4de34695a
Shoeb Farooqui <farooquishoeb2@gmail.com>
2024-11-18 20:21:19 +0530

    New Project

--- Physics Simulation/PhysicsSimulator.py
+++ Physics Simulation/PhysicsSimulator.py
@@ -5,27 +5,55 @@ import math
 
 pygame.init()
 
-WIDTH, HEIGHT = 1000,800
+WIDTH, HEIGHT = 1000,600
 PANEL = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Physics Simulation")
 
-def handleEvents():
+def handleEvents(shape):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return False
-            break
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            launchObject(shape)
+            
     return True
 
 
+def createBoundaries(space, width, height):
+    rects = [   
+        [(width/2, height - 10), (width, 20)],
+        [(width/2, 10), (width, 20)],
+        [(10, height/2), (20, height)],
+        [(width - 10, height/2), (20, height)] 
+    ]
+    
+    for pos, size in rects:
+        body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        body.position = pos
+        shape = pymunk.Poly.create_box(body, size)
+        shape.color = (48, 25, 52, 255)
+        shape.elasticity = 0.4
+        shape.friction = 0.5
+        space.add(body, shape)
+        
+        
 def createBall(space, radius, mass):
-    body = pymunk.body()
+    body = pymunk.Body()
     body.position = (WIDTH / 2, HEIGHT / 2)
-    shape = pymunk.Circle(body, 10)
+    shape = pymunk.Circle(body, radius)
     shape.mass = mass
     shape.color = (106, 90, 205, 255)
+    shape.elasticity = 1
+    shape.friction = 0.5
+    space.add(body, shape)
+    return shape
 
 
-
+def launchObject(shape):
+    shape.body.apply_impulse_at_local_point((10000, 0), (0, 0)) 
+        
+            
 def physicsSpace(window):
     space = pymunk.Space()
     space.gravity = (0, 981)
@@ -40,24 +68,23 @@ def draw(space, window, drawOptions):
     space.debug_draw(drawOptions)
     pygame.display.update()
     
-def run(window):
+    
+def run(window, width, height):
     run = True
     clock = pygame.time.Clock()
     fps = 60
     dt = 1 / fps
     
     space, drawOptions = physicsSpace(window)
+    ball = createBall(space, 30, 100)
+    createBoundaries(space, width, height)
     
     while run:
-        run = handleEvents()
+        run = handleEvents(ball)
         draw(space, window, drawOptions)
         space.step(dt)
         clock.tick(fps)
         
     pygame.quit()
-    
-run(PANEL)
-        
-    
 
-
+run(PANEL, WIDTH, HEIGHT)
