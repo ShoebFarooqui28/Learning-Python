6239d413be51b4cb99db59252c65689974b43f69
Shoeb Farooqui <farooquishoeb2@gmail.com>
2025-01-21 02:28:58 +0530

    Changes

--- Mathematical Animations/Collatz Animation/main.py
+++
@@ -1,8 +0,0 @@
-from manim import *
-
-class CC(Scene):
-    def collatzConjecture(self):
-        pass
-    def construct(self):
-        pass
-        
---
+++ NumPy/Basic/main.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+l1 = np.array([[1, 2, 3, 4, 5], [1, 5, 6, 7, 7,]])
+
+print(l1.ndim) # Dimensions
+print(l1.size) # Number of Elements
+print(l1.shape) # Rows and Columns
